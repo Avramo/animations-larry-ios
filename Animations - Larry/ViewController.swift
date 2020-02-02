bf3ea4b9683cb5d4e379086f756e30e38ed6022a
elnatan <elnatan@ravtech.co.il>
2020-02-02 17:58:43 +0200

    Initial Commit

---
+++ Animations - Larry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animations - Larry
+//
+//  Created by admin on 02/02/2020.
+//  Copyright © 2020 AM Kirsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
